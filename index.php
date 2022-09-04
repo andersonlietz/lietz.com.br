acee8b3ed51ab31de0b88db6d665c681ded0ec4d
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-09-03 21:13:05 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="css/style.css"><link rel="shortcut icon" href="favicon-16x16.png" type="image x-icon"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Bel Lietz cabeleireira</title><meta name="description" content="Bel Lietz, Cabeleireira em Cambé PR" /></head><body><header><h1>Bel Cabeleireira</h1></header><main><article><h2>Cabeleireira em Cambé PR</h2><p>Trabalhamos com variados tipos de cortes e penteados, levando em conta as preferências da cliente.</p><section id="amostras"><img src="amostra1.jpg"><img src="amostra2.jpg"><img src="amostra3.jpg"><img src="amostra4.jpg"></section></article><aside><h2>📌Endereço</h2><p>📍 Maria Jacomel Pacolla, 1249
+            <br>Jardim Ana Rosa, Cambé, PR
+            <img src="banner.jpg"></p></aside><section id="contato"><a href="https://wa.me/5541997739364"><img src="whatsapp.png"></a></section></main><footer><?php
+require "footer.php";
+?></footer></body></html> 
